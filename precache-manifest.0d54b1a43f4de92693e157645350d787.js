6dabe643284bd2455d06a0b8dc713a0b7d303d1d
Dylan <dylan.oli@hotmail.com>
2022-05-09 15:13:27 -0300

    deploy

---
+++ precache-manifest.0d54b1a43f4de92693e157645350d787.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae6f6f78e087dfd6aec",
+    "url": "/css/app.f945ddc9.css"
+  },
+  {
+    "revision": "12affdfe585871235e18",
+    "url": "/css/chunk-vendors.f1c4da89.css"
+  },
+  {
+    "revision": "54cbb332938b6e54d932b3055d4c0115",
+    "url": "/img/atendente.54cbb332.png"
+  },
+  {
+    "revision": "a6c0684812bbb04565db6555d5a42230",
+    "url": "/img/banner.a6c06848.png"
+  },
+  {
+    "revision": "e4ec282d6db92f0c6ef1227601e477b6",
+    "url": "/img/consulta.e4ec282d.png"
+  },
+  {
+    "revision": "2392258726fb1023e79481bb41818398",
+    "url": "/img/logo-white.23922587.png"
+  },
+  {
+    "revision": "558df05d7fe8fc5323311731df219e13",
+    "url": "/img/logo.558df05d.png"
+  },
+  {
+    "revision": "b3175581b25050d5dcd170a11d157317",
+    "url": "/img/medico.b3175581.png"
+  },
+  {
+    "revision": "b28e86e4806d0ad1b64171f39eaa38a2",
+    "url": "/img/paciente.b28e86e4.png"
+  },
+  {
+    "revision": "0541696ff4bca18c4eb5f4330a67f383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aae6f6f78e087dfd6aec",
+    "url": "/js/app.6bfb68d6.js"
+  },
+  {
+    "revision": "12affdfe585871235e18",
+    "url": "/js/chunk-vendors.eada7a9c.js"
+  },
+  {
+    "revision": "32fb5c5841f9ddab40d47dac8708431e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
