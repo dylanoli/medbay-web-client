d61d3aae184b93fbf782f3040b61eec1d6e93356
Dylan <dylan.oli@hotmail.com>
2022-06-02 00:43:57 -0300

    lista usuario

--- src/models/ConsultaDTO.ts
+++ src/models/ConsultaDTO.ts
@@ -1,9 +1,11 @@
+import MedicoDTO from "./MedicoDTO";
 import PessoaDTO from "./PessoaDTO";
 
 export default class ConsultaDTO {
     id = 0;
     doctorId = 0;
     patientId = 0;
+    doctor = new MedicoDTO()
     patient = new PessoaDTO()
     observations: string[] = []
     consultationDate = ''
--- src/views/HomePaciente.vue
+++ src/views/HomePaciente.vue
@@ -15,16 +15,6 @@
     <h1 class="titulo">Consultas</h1>
     <div class="content-usu">
       <div>
-        <div style="display: flex; width: 60vw">
-          <v-text-field
-            label="Pesquisar consultas"
-            prepend-icon="mdi-magnify"
-            outlined
-            dense
-            class="mr-5"
-          ></v-text-field>
-          <v-btn color="primary">buscar</v-btn>
-        </div>
         <div class="table">
           <v-data-table
             :headers="headers"
@@ -32,11 +22,6 @@
             :items-per-page="6"
             class="elevation-1"
           >
-            <template v-slot:item.action="{ item }">
-              <v-btn icon color="primary" @click="openVer()">
-                <v-icon>mdi-eye-outline</v-icon>
-              </v-btn>
-            </template>
           </v-data-table>
         </div>
       </div>
@@ -50,10 +35,16 @@
 import { Component, Vue } from "vue-property-decorator";
 
 import TopBar from "@/components/TopBar.vue";
-import Consulta from "@/models/Consulta";
 import UserDTO from "@/models/UserDTO";
+import ConsultaDTO from "@/models/ConsultaDTO";
+import ConsultaService from "@/services/ConsultaService";
+import MedicoService from "@/services/MedicoService";
 //import DialogActionUsuario from "@/components/usuario/DialogActionUsuario.vue";
-
+type Consulta = {
+  local: string;
+  medico: string;
+  data: string;
+};
 @Component({
   components: {
     TopBar,
@@ -66,51 +57,27 @@ export default class HomeUsuario extends Vue {
     return this["$store"].state.user as UserDTO;
   }
   headers: any[] = [
-    { text: "Nome", value: "nome" },
     { text: "Local", value: "local" },
-    { text: "Valor", value: "valor" },
+    { text: "Medico", value: "medico" },
     { text: "Data", value: "data" },
-    { text: " ", value: "action" },
   ];
 
-  consultas: Consulta[] = [
-    {
-      nome: "Consulta de vista",
-      local: "Rua das Flores",
-      valor: 40,
-      data: "03/04/200",
-    },
-    {
-      nome: "Consulta de vista",
-      local: "Rua das Flores",
-      valor: 40,
-      data: "03/04/200",
-    },
-    {
-      nome: "Consulta de vista",
-      local: "Rua das Flores",
-      valor: 40,
-      data: "03/04/200",
-    },
-    {
-      nome: "Consulta de vista",
-      local: "Rua das Flores",
-      valor: 40,
-      data: "03/04/200",
-    },
-    {
-      nome: "Consulta de vista",
-      local: "Rua das Flores",
-      valor: 40,
-      data: "03/04/200",
-    },
-    {
-      nome: "Consulta de vista",
-      local: "Rua das Flores",
-      valor: 40,
-      data: "03/04/200",
-    },
-  ];
+  consultas: Consulta[] = [];
+
+  async list() {
+    const list = await ConsultaService.list();
+    for (const iterator of list) {
+      iterator.doctor = await MedicoService.find(iterator.doctorId);
+    }
+    this.consultas = list.map((el) => ({
+      local: "Clínica Medbay",
+      medico: el.doctor.name,
+      data: el.consultationDate,
+    }));
+  }
+  mounted() {
+    this.list();
+  }
 }
 </script>
 
